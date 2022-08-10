40b2acaa7d20a86cf4702f360c5dd7817ce39c03
lijinchao98 <41536921+lijinchao98@users.noreply.github.com>
2022-08-10 22:38:18 +0800

    .

---
+++ model.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import dataset
+from part import head_block, backbone_block,dense_block
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.head = head_block()
+        self.backbone = backbone_block()
+        self.dense = dense_block()
+
+    def forward(self, x):
+        x = self.head(x)
+        x = self.backbone(x)
+        x = self.dense(x)
+
+        return x
+
+if __name__ == '__main__':
+
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    net = Net()
+    net.to(device=device)
+
+    from torchsummary import summary
+    summary(net, input_size=(1, 189)) # (channel, H, W)
+
+    # 指定训练集地址，开始训练
+    HSI_dataset = dataset.HSI_Loader('water_10000.npy')
+    train_loader = torch.utils.data.DataLoader(dataset=HSI_dataset,
+                                               batch_size=1024,
+                                               shuffle=True)
+    for curve, label in train_loader:
+        # 将数据拷贝到device中
+        curve = curve.unsqueeze(1).to(device=device, dtype=torch.float32)
+        label = label.to(device=device, dtype=torch.float32)
+        break
+    print(curve.shape)
+    out = net(curve)
+    print(out.shape)
--- part.py
+++ part.py
@@ -3,33 +3,41 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def bn_relu(channels, sequential, dropout=0, order=''):
+    """bn => relu [=>drop out]"""
+    net = sequential
+    net.add_module(str(order)+'bn', nn.BatchNorm1d(channels))
+    net.add_module(str(order)+'relu', nn.ReLU(inplace=True))
+    if dropout:
+        net.add_module(str(order)+'dropout', nn.Dropout(p=0.2))
+
 class resnet_block(nn.Module):
+    # x5
     """res = _bn_relu => conv => _bn_relu + drop out => conv"
        out =  res + shorcut(input)
     """
-    def __init__(self):
+    def __init__(self, in_channel, zero_pad, sequential, order=''):
         super().__init__()
 
-        res = nn.Sequential()
+        res = sequential
+        zero_pad = zero_pad
+        if zero_pad:
+            out_channel = in_channel*2
+        else: out_channel = in_channel
 
-        def bn_relu(channels, dropout=0):
-            """bn => relu [=>drop out]"""
-            res.add_module(nn.BatchNorm1d(channels))
-            res.add_module(nn.ReLU(inplace=True))
-            if dropout:
-                res.add_module(nn.Dropout(p=0.2))
-
-        bn_relu(channels)
-        res.add_module(nn.Conv1d(in_channle, out_channel, kernel_size=16, padding=1))
-        bn_relu(channels, dropout=1)
-        res.add_module(nn.Conv1d(in_channle, out_channel, kernel_size=16, padding=1))
+        bn_relu(in_channel, res, order=order)
+        res.add_module(str(order)+'conv1', nn.Conv1d(in_channel, in_channel, kernel_size=3, stride=1, padding=1, bias=False))
+        bn_relu(in_channel, res, dropout=1, order=str(order)+'+')
+        res.add_module(str(order)+'conv2', nn.Conv1d(in_channel, out_channel, kernel_size=3, stride=2, padding=1))
 
         self.res = res
-        self.shortcut = nn.Maxpool1d(2)
+        self.shortcut = nn.MaxPool1d(2, padding=1, dilation=2)
+        self.zero_pad = zero_pad
 
-    def forward(self, x, zero_pad=0):
+    def forward(self, x):
         res = self.res(x)
         shortcut = self.shortcut(x)
+        zero_pad = self.zero_pad
 
         def zeropad(x):
             y = torch.zeros_like(x)
@@ -42,4 +50,75 @@ class resnet_block(nn.Module):
 
         return out
 
-class 
+class head_block(nn.Module):
+    """head_1: conv => _bn_relu
+       head_2 = conv => _bn_relu + dropout => conv
+       out = head_2( head_1(input) ) + shortcut( head_1(input) )
+    """
+    def __init__(self):
+        super().__init__()
+
+        head_1 = nn.Sequential()
+
+        head_1.add_module('conv1',nn.Conv1d(1, 32, kernel_size=3, stride=1, padding=1, bias=False))
+        bn_relu(32, head_1)
+
+        head_2 = nn.Sequential()
+
+        head_2.add_module('conv2', nn.Conv1d(32, 32, kernel_size=3, stride=1, padding=1, bias=False))
+        bn_relu(32, head_2, dropout=1)
+        head_2.add_module('conv3', nn.Conv1d(32, 32, kernel_size=3, stride=2, padding=1))
+
+        self.head_1 = head_1
+        self.head_2 = head_2
+        self.shortcut = nn.MaxPool1d(2, padding=1, dilation=2)
+
+    def forward(self, x):
+        x = self.head_1(x)
+        x = self.head_2(x) + self.shortcut(x)
+
+        return x
+
+class backbone_block(nn.Module):
+    """resnet_block x5
+    """
+    def __init__(self):
+        super().__init__()
+        backbone = nn.Sequential()
+
+        start_channel = 32
+        for i in range(5):
+            zero_pad = i%2  # 奇数变通道数，需要pad，concat，奇数除2刚好余1，zero_pad为true
+            resnet_block(start_channel, zero_pad, backbone, order=i)
+            if zero_pad:
+                start_channel = start_channel*2
+
+        self.backbone = backbone
+
+    def forward(self, x):
+        x = self.backbone(x)
+
+        return x
+
+class dense_block(nn.Module):
+    """ _bn_relu => dense => softmax
+    """
+    def __init__(self):
+        super().__init__()
+
+        dense = nn.Sequential()
+
+        bn_relu(128, dense)
+        dense.add_module('flatten', nn.Flatten())
+        dense.add_module('linear1', nn.Linear(384, 128))
+        dense.add_module('linear2', nn.Linear(128, 64))
+        dense.add_module('linear3', nn.Linear(64, 32))
+        dense.add_module('linear4', nn.Linear(32, 16))
+        dense.add_module('linear5', nn.Linear(16, 2))
+
+        self.dense = dense
+
+    def forward(self, x):
+        x = self.dense(x)
+
+        return x
