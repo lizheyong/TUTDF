50940a1e1cbc34d2314fe76a7a7978093e42368a
lijinchao98 <41536921+lijinchao98@users.noreply.github.com>
2022-08-29 23:01:43 +0800

    。

--- model/model.py
+++ model/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from dataset import dataset
-from .part import *
+from part import *
 
 
 class ResNet(nn.Module):
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    net = Net()
+    net = ResNet()
     net.to(device=device)
 
     from torchsummary import summary
